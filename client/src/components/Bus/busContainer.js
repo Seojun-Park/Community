7d4bcc075d0173adc3ea1f611e6afb60133f5308
Jinchul park <jinchul112@gmail.com>
2020-05-04 01:18:37 +0200

    update

--- client/src/components/Bus/busContainer.js
+++ client/src/components/Bus/busContainer.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 
-const url = "cgi-bin/rtpi/realtimebusinformation?stopid=184&format=json";
+const url = `cgi-bin/rtpi/realtimebusinformation?stopid=184&format=json`
 
 export default () => {
   const [busData, setBusData] = useState([]);
-  const callApi = () => {
-    fetch(url)
+  const callApi = async() => {
+    await fetch(url)
       .then(data => data.json())
       .then(json => setBusData(json.data))
       .catch(e => {
