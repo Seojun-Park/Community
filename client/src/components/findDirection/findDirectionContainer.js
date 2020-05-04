daec1e1a40a957854988be33e40272c3f8af2688
Jinchul park <jinchul112@gmail.com>
2020-05-05 01:26:01 +0200

    update

--- client/src/components/findDirection/findDirectionContainer.js
+++ client/src/components/findDirection/findDirectionContainer.js
@@ -1,31 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Map } from "google-maps-react";
 
-const Wrapper = styled.div`
-  max-width: 100vw;
-`;
+import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { MAP_TOKEN } from "../../key";
+import { debounce } from "@material-ui/core";
+import { MarkerIcon } from "../Icon";
 
-const MapContainer = styled.div`
-  position: relative;
-  background-size: cover;
+const Wrapper = styled.div``;
+
+const NaviControl = styled.div`
+  position: absolute;
 `;
 
 export default () => {
-  const mapStyles = {
-    width: "500px",
-    height: "500px"
-  };
+  const [viewport, setViewport] = useState({
+    latitude: 53.347614,
+    longitude: -6.259293,
+    width: "30vw",
+    height: "60vh",
+    zoom: 12
+  });
+
+  useEffect(() => {
+    const resizeEventListener = window.addEventListener(
+      "resize",
+      debounce(() => {
+        setViewport({
+          ...viewport,
+          width: window.innerWidth,
+          height: window.innerHeight
+        });
+      }, 200)
+    );
+    return () => {
+      window.removeEventListener("resize", resizeEventListener);
+    };
+  }, []);
   return (
     <Wrapper>
-      <MapContainer>
-        <Map
-          google={window.google}
-          style={mapStyles}
-          zoom={8}
-          initialCenter={{ lat: 0, lng: 0 }}
-        />
-      </MapContainer>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={MAP_TOKEN}
+        mapStyle="mapbox://styles/mapbox/light-v10"
+        onViewportChange={setViewport}
+      >
+        <NaviControl>
+          <NavigationControl />
+        </NaviControl>
+        <Marker latitude={53.347614} longitude={-6.259293}>
+          <MarkerIcon />
+        </Marker>
+      </ReactMapGL>
     </Wrapper>
   );
 };
