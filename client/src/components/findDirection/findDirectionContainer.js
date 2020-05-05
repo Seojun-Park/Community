659053be3c63104899e18db2e954031abc09809e
Jinchul park <jinchul112@gmail.com>
2020-05-05 17:07:48 +0200

    still map

--- client/src/components/findDirection/findDirectionContainer.js
+++ client/src/components/findDirection/findDirectionContainer.js
@@ -1,45 +1,89 @@
-import React, { useState } from "react";
-import ReactMapGL, { SVGOverlay } from "react-map-gl";
-// import { fromJS } from 'immutable'
+import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import React, { Component } from "react";
+import MapGL, { GeolocateControl } from "react-map-gl";
+import DeckGL, { GeoJsonLayer } from "deck.gl";
+import Geocoder from "react-map-gl-geocoder";
 import { MAP_TOKEN } from "../../key";
 
-export default () => {
-  const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
-  });
+const token = MAP_TOKEN;
 
-  const redraw = ({ project }) => {
-    const [cx, cy] = project([-122.4, 37.7]);
-    return <circle cx={cx} cy={cy} r={4} fill="blue" />;
-  };
-
-  const goToNYC = () => {
-    const viewport = { ...viewport, longitude: -74.1, latitude: 40.7 };
-    setViewport(viewport);
-  };
-
-  const onViewportChange = viewport => {
-    if (viewport.longitude > 0) {
-      viewport.longitude = 0;
-    }
-    setViewport(viewport);
-  };
-
-  return (
-    <>
-      <button onClick={goToNYC}>NYC</button>
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken={MAP_TOKEN}
-        mapStyle="mapbox://styles/mapbox/light-v10"
-        onViewportChange={onViewportChange}
-      >
-        <SVGOverlay redraw={redraw} />
-      </ReactMapGL>
-    </>
-  );
+const geolocateStyle = {
+  float: "left",
+  margin: "50px",
+  padding: "10px"
 };
+class SearchableMap extends Component {
+  state = {
+    viewport: {
+      latitude: 53.347614,
+      longitude: -6.259293,
+      zoom: 12
+    },
+    searchResultLayer: null
+  };
+
+  mapRef = React.createRef();
+
+  handleViewportChange = viewport => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    });
+  };
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  handleGeocoderViewportChange = viewport => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+    return this.handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides
+    });
+  };
+
+  handleOnResult = event => {
+    this.setState({
+      searchResultLayer: new GeoJsonLayer({
+        id: "search-result",
+        data: event.result.geometry,
+        getFillColor: [255, 0, 0, 128],
+        getRadius: 1000,
+        pointRadiusMinPixels: 10,
+        pointRadiusMaxPixels: 10
+      })
+    });
+  };
+
+  render() {
+    const { viewport, searchResultLayer } = this.state;
+    return (
+      <div style={{ height: "100vh" }}>
+        <MapGL
+          ref={this.mapRef}
+          {...viewport}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          width="70%"
+          height="80%"
+          onViewportChange={this.handleViewportChange}
+          mapboxApiAccessToken={token}
+        >
+          <Geocoder
+            mapRef={this.mapRef}
+            onResult={this.handleOnResult}
+            onViewportChange={this.handleGeocoderViewportChange}
+            mapboxApiAccessToken={token}
+            position="top-left"
+          />
+          <GeolocateControl
+            style={geolocateStyle}
+            positionOptions={{ enableHighAccuracy: true }}
+            trackUserLocation={true}
+          />
+        </MapGL>
+        <DeckGL {...viewport} layers={[searchResultLayer]} />
+      </div>
+    );
+  }
+}
+
+export default SearchableMap;
+
