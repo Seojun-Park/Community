2be9b20be5849cdc1b4f210f82e37f360f74e494
Jinchul park <jinchul112@gmail.com>
2020-05-06 23:22:54 +0200

    working

--- client/src/components/findDirection/TransporInfo.js
+++ client/src/components/findDirection/TransporInfo.js
@@ -81,6 +81,8 @@ export default () => {
   });
 
 //   버스 api 쿼리문 만들어야함 stopid=[number] || route=[number] ...etc
+// 루트로 선택 시 스테이션 위치 보여주고 클릭하면 스테이션 정보를 가져와보자 ㅇㅋㅇㅋ
+
   const handleOnChange = e => {
     if (e.target.value === "Bus") {
       setAction("Bus");
