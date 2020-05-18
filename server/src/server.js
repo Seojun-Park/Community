7dffbd0da0fef960a4ab4e2d4dfd5a5acef9c9c4
Jinchul park <jinchul112@gmail.com>
2020-05-19 00:15:50 +0200

    api test

--- server/src/server.js
+++ server/src/server.js
@@ -6,9 +6,6 @@ import { authenticateJwt } from "./passport";
 import { isAuthenticated } from "./middlewares";
 import * as firebase from "firebase/app";
 
-const naverClient_Id = process.env.NAVER_CLIENT_ID;
-const naverSecret = process.env.NAVER_CLIENT_SECRET;
-
 const server = new GraphQLServer({
   schema,
   context: ({ request }) => ({ request, isAuthenticated })
@@ -16,32 +13,33 @@ const server = new GraphQLServer({
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
-
-server.express.get("/search/blog", function(req, res) {
+server.express.get("search/blog", function(req, res) {
   var api_url =
-    "https://openapi.naver.com/v1/search/blog.json?query=" +
-    encodeURI(req.query.query); // json 결과
+    "https://openapi.naver.com/v1/search/blog?query=" +
+    encodeURI(req.query.query);
   var request = require("request");
   var options = {
     url: api_url,
     headers: {
-      "X-Naver-Client-Id": client_id,
-      "X-Naver-Client-Secret": client_secret
+      "X-Naver-Client-Id": process.env.NAVER_CLIENT_ID,
+      "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET
     }
   };
   request.get(options, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
+    if (!error && response.statusCode === 200) {
       res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
       res.end(body);
     } else {
       res.status(response.statusCode).end();
-      console.log("error = " + response.statusCode);
+      console.log("error =  " + response.statusCode);
     }
   });
 });
 
-server.express.listen(3000, function(){
-  console.log('http://127.0.0.1:3000/search/blog?query=검색어 app listening on port 3000!');
-})
+server.express.listen(3000, function() {
+  console.log(
+    "http://127.0.0.1:3000/search/blog?query=검색어 app listening on port 3000!"
+  );
+});
 
 server.start(() => console.log(`server running`));
