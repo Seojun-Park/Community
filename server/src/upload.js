4c3dcf42f03534095fdfbaccfe49d2bb59fe0dbe
Jinchul park <jinchul112@gmail.com>
2020-05-21 14:39:47 +0200

    upload

--- server/src/upload.js
+++ server/src/upload.js
@@ -1,11 +1,26 @@
 import multer from "multer";
+import { Storage } from "@google-cloud/storage";
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({
+  dest: "uploads/",
+  // storage: multer.memoryStorage(),
+  // limits: {
+  //   fileSize: 5 * 2048 * 2048
+  // }
+});
+
+// const bucketName = "gs://dublincommunity-55071.appspot.com";
 
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
   const { file } = req;
-  console.log(file);
+  // storage.bucket(bucketName).upload(file.filename, {
+  //   gzip: true,
+  //   metadata: {
+  //     cacheControl: "public, max-age=31536000"
+  //   }
+  // });
+  console.log(req.file);
   res.json(file);
 };
