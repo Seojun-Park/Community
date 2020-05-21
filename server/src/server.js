86b56ee989fc68000f0fcab3afc6a9dc6b27cadb
Jinchul park <jinchul112@gmail.com>
2020-05-21 12:30:09 +0200

    upload

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,12 @@ import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import schema from "./schemas";
 import logger from "morgan";
+import { uploadMiddleware, uploadController } from "./upload"
+
 import { authenticateJwt } from "./passport";
 import { isAuthenticated } from "./middlewares";
+
+const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
   schema,
@@ -12,5 +16,8 @@ const server = new GraphQLServer({
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
+server.express.post("/upload", uploadMiddleware, uploadController);
 
-server.start(() => console.log(`server running`));
+server.start({ port: PORT }, () =>
+  console.log(`server running on Port ${PORT}`)
+);
