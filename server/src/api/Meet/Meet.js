d57d1f9f0db30ba31bd1c80afaf5da8f4b5532b5
Jinchul park <jinchul112@gmail.com>
2020-05-30 14:10:58 +0200

    update

--- server/src/api/Meet/Meet.js
+++ server/src/api/Meet/Meet.js
@@ -3,6 +3,6 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Meet: {
     participants: ({ id }) => prisma.meet({ id }).participants(),
-    images: ({id}) => prisma.meet({id}).images()
+    images: ({ id }) => prisma.meet({ id }).images()
   }
 };
