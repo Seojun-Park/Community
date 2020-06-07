69b37de55eda39316b504ec2b5f13a6da8f5f06d
Jinchul park <jinchul112@gmail.com>
2020-06-07 11:57:43 +0200

    update

--- server/src/api/Meet/editMeet/editMeet.js
+++ server/src/api/Meet/editMeet/editMeet.js
@@ -4,7 +4,15 @@ export default {
   Mutation: {
     editMeet: (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
-      const { id, intro, location, creator } = args;
+      const {
+        id,
+        intro,
+        location,
+        creator,
+        date,
+        isPublic,
+        maxparticipants
+      } = args;
       const { user } = reuqest;
       if (creator !== user.id) {
         console.log("You are not Master of this meeting");
@@ -16,7 +24,10 @@ export default {
           },
           data: {
             intro,
-            location
+            location,
+            date,
+            isPublic,
+            maxparticipants
           }
         });
       }
