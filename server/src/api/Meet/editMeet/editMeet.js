dcc433667fa64ec7d101c611b752fdee1ac5b25c
Jinchul park <jinchul112@gmail.com>
2020-06-07 11:03:39 +0200

    update

---
+++ server/src/api/Meet/editMeet/editMeet.js
@@ -0,0 +1,25 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    editMeet: (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { id, intro, location, creator } = args;
+      const { user } = reuqest;
+      if (creator !== user.id) {
+        console.log("You are not Master of this meeting");
+        return null;
+      } else {
+        return prisma.updateMeet({
+          where: {
+            id
+          },
+          data: {
+            intro,
+            location
+          }
+        });
+      }
+    }
+  }
+};
