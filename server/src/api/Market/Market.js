73fe361997481b310040cf2616538b334e8d718c
Jinchul park <jinchul112@gmail.com>
2020-06-15 00:57:46 +0200

    up

--- server/src/api/Market/Market.js
+++ server/src/api/Market/Market.js
@@ -3,7 +3,6 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Market: {
     user: parent => prisma.market({ id: parent.id }).user(),
-    comments: parent => prisma.market({ id: parent.id }).comments(),
     images: parent => prisma.market({ id: parent.id }).images()
   },
   Image: {
--- server/src/api/Rent/Rent.js
+++ server/src/api/Rent/Rent.js
@@ -2,7 +2,6 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   Rent: {
-    user: ({ id }) => prisma.rent({ id }).user(),
-    comments: ({ id }) => prisma.rent({ id }).comments()
+    user: ({ id }) => prisma.rent({ id }).user()
   }
 };
