3e9593c9cf84cef90dd2c804542deba11f7b1d80
Jinchul park <jinchul112@gmail.com>
2020-06-19 16:09:58 +0200

    up

--- server/src/api/Meet/searchTheme/searchTheme.js
+++ server/src/api/Meet/searchTheme/searchTheme.js
@@ -2,11 +2,13 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Query: {
-    searchTheme: async (_, args) =>
-      await prisma.meets({
+    searchTheme: async (_, args) => {
+      console.log(args);
+      return await prisma.meets({
         where: {
           theme_contains: args.theme
         }
-      })
+      });
+    }
   }
 };
